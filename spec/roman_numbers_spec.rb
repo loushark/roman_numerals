f7c211c7345a2fb1e33923824b502d1c2ae2bc9b
loushark <lou.m.reade@gmail.com>
2021-02-26 11:07:43 +0000

    added test (996 returns CMXCVI)

--- spec/roman_numbers_spec.rb
+++ spec/roman_numbers_spec.rb
@@ -17,4 +17,8 @@ describe RomanNumerals do
     expect(RomanNumerals.convert(35)).to eq "XXXV"
   end
 
+  it "should be able to convert 996 to CMXCVI" do
+    expect(RomanNumerals.convert(996)).to eq "CMXCVI"
+  end
+
 end
