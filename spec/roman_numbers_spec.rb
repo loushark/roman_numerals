60fa7eaf01e49d5ac7ddd523bb584aecec4b1fe3
loushark <lou.m.reade@gmail.com>
2021-02-26 10:46:48 +0000

    added test (5 returns V)

--- spec/roman_numbers_spec.rb
+++ spec/roman_numbers_spec.rb
@@ -1,5 +1,12 @@
+require 'roman_numerals'
+
 describe RomanNumerals do
   it "should be able to conver 1 to I" do
     expect(RomanNumerals.convert(1)).to eq ("I")
   end
+
+  it "should be able to convert 5 to V" do
+    expect(RomanNumerals.convert(5)).to eq("V")
+  end
+
 end
