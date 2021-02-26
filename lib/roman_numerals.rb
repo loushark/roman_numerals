baa14624d29116a67f3cfe42f0e625dbd3783b67
loushark <lou.m.reade@gmail.com>
2021-02-26 10:52:53 +0000

    passed test (10 returns X)

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,6 +1,9 @@
 class RomanNumerals
+  ROMAN_NUMBERALS = {
+    1 => "I", 5 => "V", 10 => "X"
+  }
+
   def self.convert(number)
-    return "V" if number == 5
-    "I"
+    ROMAN_NUMBERALS[number]
   end
 end
