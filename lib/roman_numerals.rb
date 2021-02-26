4cd93365182c17531b26b2f6c20d08b8688c0cf1
loushark <lou.m.reade@gmail.com>
2021-02-26 11:14:55 +0000

    passed test (996 retruns CMXCVI)

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,12 +1,15 @@
 class RomanNumerals
   ROMAN_NUMBERALS = {
-    10 => "X", 5 => "V", 1 => "I",
+    1000 => "M", 900 => "CM", 500 => "D",
+    400 => "CD", 100 => "C", 90 => "XC",
+    40 => "XL", 10 => "X", 9 => "IX",
+    5 => "V", 4 => "IV", 1 => "I",
   }
 
   def self.convert(number)
     roman_numeral = ""
     ROMAN_NUMBERALS.each do |k, v|
-      (number / k).times {roman_numeral << v; number = number - k }
+      (number / k).times {roman_numeral << v; number -= k }
     end
     roman_numeral
   end
